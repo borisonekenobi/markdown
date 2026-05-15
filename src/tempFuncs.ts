b3e0ecc982b19ef2effd5abb9ff4d9fcaaa1a5cd
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-05-14 22:41:46 -0400

    Fix inline-HTML parsing and definition lists

--- src/tempFuncs.ts
+++ src/tempFuncs.ts
@@ -1,17 +1,27 @@
 import {unified} from 'unified';
 import remarkParse from 'remark-parse';
 import remarkGfm from 'remark-gfm';
-import remarkHtml from 'remark-html';
+import remarkRehype from 'remark-rehype';
+import rehypeRaw from 'rehype-raw';
+import rehypeStringify from 'rehype-stringify';
 import remarkStringify from 'remark-stringify';
 import rehypeParse from 'rehype-parse';
 import rehypeRemark from 'rehype-remark';
+import {
+	defListHastHandlers,
+	remarkDefinitionList,
+} from 'remark-definition-list';
 
 // Parse markdown -> HTML string
 export async function tmpParse(markdown: string): Promise<string> {
 	const vfile = await unified().
 		use(remarkParse).
+		use(remarkDefinitionList).
 		use(remarkGfm).
-		use(remarkHtml).
+		use(remarkRehype,
+			{allowDangerousHtml: true, handlers: defListHastHandlers}).
+		use(rehypeRaw).
+		use(rehypeStringify).
 		process(markdown);
 
 	return String(vfile);
@@ -19,13 +29,10 @@ export async function tmpParse(markdown: string): Promise<string> {
 
 // Serialize HTML string -> markdown
 export async function tmpSerialize(html: string): Promise<string> {
-	const vfile = await unified()
-		// parse the HTML input into a rehype tree
-		.use(rehypeParse, {fragment: true})
-		// convert rehype (HTML) to remark (Markdown) AST
-		.use(rehypeRemark)
-		// support GitHub-flavored Markdown when stringifying
-		.use(remarkGfm).use(remarkStringify).process(html);
+	const vfile = await unified().
+		use(rehypeParse, {fragment: true}).
+		use(rehypeRemark).
+		use(remarkGfm).use(remarkStringify).process(html);
 
 	return String(vfile);
 }
